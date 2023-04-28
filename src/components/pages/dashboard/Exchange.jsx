675fc041880be9371e15aee06da1d0f7cf2decd8
mojtaba <mojtaba75rs@gmail.com>
2023-04-28 23:11:10 +0430

    Edit the Exchange component

--- src/components/pages/dashboard/Exchange.jsx
+++ src/components/pages/dashboard/Exchange.jsx
@@ -7,35 +7,40 @@ import {
   Autocomplete,
   TextField,
   Button,
+  Select,
+  MenuItem,
 } from "@mui/material";
 import { Sync, TrendingFlat } from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { memo, useEffect, useMemo, useState } from "react";
+import { fetchExchangeCoin, fetchExchanges } from "../../../reducers/coinSlice";
 
 const Exchange = () => {
   const theme = useTheme();
 
-  const countries = [
-    { code: "AD", label: "Andorra", phone: "376" },
-    {
-      code: "AE",
-      label: "United Arab Emirates",
-      phone: "971",
-    },
-    { code: "AF", label: "Afghanistan", phone: "93" },
-    {
-      code: "AG",
-      label: "Antigua and Barbuda",
-      phone: "1-268",
-    },
-    { code: "AI", label: "Anguilla", phone: "1-264" },
-    { code: "AL", label: "Albania", phone: "355" },
-    { code: "AM", label: "Armenia", phone: "374" },
-  ];
+  const data = useSelector((state) => state.cryptoCurrency.exchangeCoin);
+
+  const [selectedCoin, setSelectedCoin] = useState();
+
+  const dispatch = useDispatch();
+
+  const exchanges = useSelector((state) => state.cryptoCurrency.exchanges);
+
+  const handleSelectExchange = (id) => {
+    setSelectedCoin(id);
+    dispatch(fetchExchangeCoin(id));
+  };
+
+  useEffect(() => {
+    dispatch(fetchExchanges());
+  }, []);
 
   return (
     <Box
       sx={{
-        width: "340px",
-        height: "340px",
+        width: 1,
+        maxWidth: "460px",
+        minHeight: "340px",
         backgroundColor: "background.paper",
         border: `1px solid ${
           theme.palette.mode === "dark" ? "#303241" : "#DADADC"
@@ -64,128 +69,102 @@ const Exchange = () => {
             alignItems: "center",
           }}
         >
-          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-            <Typography variant="caption">1</Typography>
-            <Typography
-              variant="body2"
-              sx={{ color: "#666873", letterSpacing: 1 }}
-            >
-              BTC
-            </Typography>
-          </Box>
-          <TrendingFlat />
-          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-            <Typography variant="body2" sx={{ letterSpacing: 1 }}>
-              $ 27,597.55
-            </Typography>
-            <Typography
-              variant="caption"
-              sx={{ color: "#666873", letterSpacing: 1 }}
-            >
-              USDT
-            </Typography>
-          </Box>
-        </Box>
-        <Box sx={{ mt: "10px" }}>
-          <Typography sx={{ color: "#666873" }}>Get</Typography>
           <Autocomplete
-            id="country-select-demo"
             sx={{
-              maxWidth: 300,
-              mt: "4px",
-              borderRadius: "12px",
-              "& .MuiOutlinedInput-root": {
-                outlineColor: "none",
-                borderRadius: "12px",
-                height: "50px !important",
-              },
-            }}
-            options={countries}
-            autoHighlight
-            getOptionLabel={(option) => option.label}
-            renderOption={(props, option) => (
-              <Box
-                component="li"
-                sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
-                {...props}
-              >
-                <img
-                  loading="lazy"
-                  width="20"
-                  src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
-                  srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
-                  alt=""
-                />
-                {option.label} ({option.code}) +{option.phone}
-              </Box>
-            )}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                inputProps={{
-                  ...params.inputProps,
-                  autoComplete: "new-password", // disable autocomplete and autofill
-                }}
-              />
-            )}
-          />
-          <Typography sx={{ mt: "8px", color: "#666873" }}>Pay</Typography>
-          <Autocomplete
-            id="country-select-demo"
-            sx={{
-              maxWidth: 300,
-              mt: "4px",
-              borderRadius: "12px",
-              "& .MuiOutlinedInput-root": {
-                outlineColor: "none",
-                borderRadius: "12px",
-                height: "50px !important",
-              },
-            }}
-            options={countries}
-            autoHighlight
-            getOptionLabel={(option) => option.label}
-            renderOption={(props, option) => (
-              <Box
-                component="li"
-                sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
-                {...props}
-              >
-                <img
-                  loading="lazy"
-                  width="20"
-                  src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
-                  srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
-                  alt=""
-                />
-                {option.label} ({option.code}) +{option.phone}
-              </Box>
-            )}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                inputProps={{
-                  ...params.inputProps,
-                  autoComplete: "new-password", // disable autocomplete and autofill
-                }}
-              />
-            )}
-          />
-          <Button
-            sx={{
-              width: "300px",
-              height: "50px",
+              width: 1,
+              // maxWidth: 308,
               mt: "12px",
               borderRadius: "12px",
-              background:
-                "linear-gradient(108.49deg, #AF53FF 0%, #6EACFE 95.17%)",
-              color: "#fff",
-              textTransform: "capitalize",
+              "& .MuiOutlinedInput-root": {
+                outlineColor: "none",
+                borderRadius: "12px",
+                height: "50px !important",
+              },
             }}
-          >
-            Exchange
-          </Button>
+            options={exchanges}
+            autoHighlight
+            onChange={(event, newValue) => {
+              handleSelectExchange(newValue.id);
+            }}
+            getOptionLabel={(option) => option.name}
+            renderOption={(props, option) => (
+              <Box
+                component="li"
+                sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
+                {...props}
+              >
+                <Typography variant="body2" sx={{ mr: 2 }}>
+                  {option.trust_score_rank}
+                </Typography>
+                <img
+                  loading="lazy"
+                  width="30"
+                  src={option.image}
+                  srcSet={`${option.image} 2x`}
+                  alt={option.name}
+                  style={{ borderRadius: "50%" }}
+                />
+                {option.name}
+              </Box>
+            )}
+            renderInput={(params) => <TextField label="exchange" {...params} />}
+          />
         </Box>
+        {selectedCoin &&
+          data.tickers?.slice(0, 3).map((item, index) => (
+            <Box
+              key={index}
+              sx={{
+                width: 1,
+                // maxWidth: "308px",
+                minHeight: "50px",
+                display: "flex",
+                flexWrap: "wrap",
+                alignItems: "center",
+                gap: 1,
+                mt: 2,
+                padding: "8px 16px",
+                border: `1px solid ${
+                  theme.palette.mode === "dark" ? "#303241" : "#DADADC"
+                }`,
+                borderRadius: "12px",
+                "&:hover": { backgroundColor: "action.hover" },
+              }}
+            >
+              <Box sx={{ display: "flex", alignItems: "stretch", gap: 1 }}>
+                <Typography
+                  variant="body2"
+                  sx={{ textTransform: "capitalize", letterSpacing: 1 }}
+                >
+                  {item.coin_id}
+                </Typography>
+                <Typography
+                  variant="caption"
+                  sx={{ color: "#666873", letterSpacing: 1 }}
+                >
+                  {item.base}
+                </Typography>
+              </Box>
+              <TrendingFlat />
+              <Box sx={{ display: "flex", alignItems: "stretch", gap: 1 }}>
+                <Typography variant="body2" sx={{ color: "#666873" }}>
+                  ${item.last}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{ textTransform: "capitalize", letterSpacing: 1 }}
+                >
+                  {item.target_coin_id}
+                </Typography>
+                <Typography
+                  variant="caption"
+                  sx={{ color: "#666873", letterSpacing: 1 }}
+                >
+                  {item.target}
+                </Typography>
+              </Box>
+            </Box>
+          ))}
       </Box>
     </Box>
   );
