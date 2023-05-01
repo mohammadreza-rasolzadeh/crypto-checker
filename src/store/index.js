8c19d9648f692bc424eda307a3811f12cc75d18f
mojtaba <mojtaba75rs@gmail.com>
2023-05-01 20:10:08 +0430

    Fetch information in the store file

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,13 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
-import cryptoReducer, { fetchTrendingCoins } from "../reducers/coinSlice";
-import { cryptoApi, useGetTrendingCoinsQuery } from "../reducers/coinsApi";
+import cryptoReducer, {
+    fetchCategoriesList,
+    fetchCoinList,
+    fetchExchanges,
+    fetchGlobalCryptoCurrencyEecentralized,
+    fetchTrendingCoins,
+} from "../reducers/coinSlice";
+import { cryptoApi } from "../reducers/coinsApi";
 
 export const store = configureStore({
     reducer: {
@@ -12,5 +18,8 @@ export const store = configureStore({
         getDefaultMiddleware().concat(cryptoApi.middleware),
 });
 
-// store.dispatch(fetchTrendingCoins());
-// store.dispatch(useGetTrendingCoinsQuery)
+store.dispatch(fetchTrendingCoins());
+store.dispatch(fetchCoinList());
+store.dispatch(fetchCategoriesList());
+store.dispatch(fetchGlobalCryptoCurrencyEecentralized());
+store.dispatch(fetchExchanges());
